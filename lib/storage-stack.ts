9fabee8cfc444ba0e437d65f272818de7ea98429
n0928518 <n0928518@my.ntu.ac.uk>
2023-10-20 15:24:45 +0100

    micro test

--- lib/storage-stack.ts
+++ lib/storage-stack.ts
@@ -6,7 +6,7 @@ export class StorageStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new aws_s3.Bucket(this, 'MyFirstBucket', {
+        new aws_s3.Bucket(this, 'MyFirstBucket2', {
             versioned: true,
             removalPolicy: cdk.RemovalPolicy.DESTROY
         });
