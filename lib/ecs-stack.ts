9a528bb57faa16ffe6f0eb64caf076d71dfff47b
n0928518 <n0928518@my.ntu.ac.uk>
2023-10-24 12:23:23 +0100

    cheaper 2

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -8,25 +8,41 @@ export class EcsStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Create a new VPC
-        const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 1 });
+        // Create a new VPC with a specific CIDR block
+        const vpc = new ec2.Vpc(this, 'MyVpc', {
+            maxAzs: 2,
+        });
 
         // Create an ecs cluster
-        const cluster = new ecs.Cluster(this, 'FargateCluster', { vpc });
+        const cluster = new ecs.Cluster(this, 'Ec2Cluster', { vpc });
+
+        // Add capacity to the cluster
+        cluster.addCapacity('DefaultAutoScalingGroup', {
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+        });
 
         // Specify the path to your Dockerfile
         const dockerfileDirectory = 'src/ecs/';
 
-        // Create a Fargate service with an application load balancer
-        const fargateService=  new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+        // Create a task definition with a single container
+        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
+        const webContainer = taskDefinition.addContainer('web', {
+            image: ecs.ContainerImage.fromAsset(dockerfileDirectory),
+            memoryLimitMiB: 512,
+        });
+
+        webContainer.addPortMappings({
+            containerPort: 80,
+        });
+
+        // Create an EC2 service with a network load balancer
+        const ec2Service = new ecs_patterns.NetworkLoadBalancedEc2Service(this, 'MyEc2Service', {
             cluster,
-            taskImageOptions: {
-                image: ecs.ContainerImage.fromAsset(dockerfileDirectory),
-            },
+            taskDefinition,
             publicLoadBalancer: true,
         });
 
         // Output the DNS name of the load balancer
-        new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
+        new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: ec2Service.loadBalancer.loadBalancerDnsName });
     }
 }
