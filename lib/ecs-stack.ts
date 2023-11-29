d24df93cc7761a21aa9493f81d4234b3f6bb934c
n0928518 <n0928518@my.ntu.ac.uk>
2023-11-29 13:52:50 +0000

    attempting to use a t4g nano

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from "constructs";
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as ecr from "aws-cdk-lib/aws-ecr";
 
 export class EcsStack extends cdk.Stack {
@@ -19,17 +19,45 @@ export class EcsStack extends cdk.Stack {
             vpc: vpc
         });
 
+        // Define an Auto Scaling group with t4g.nano instances
+        const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'ASG', {
+            vpc,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO), // Corrected to T4G.NANO
+            machineImage: ecs.EcsOptimizedImage.amazonLinux2(ecs.AmiHardwareType.ARM),
+            minCapacity: 1,
+            maxCapacity: 2
+        });
+
+        // Create an ECS Capacity Provider using the Auto Scaling group
+        const capacityProvider = new ecs.AsgCapacityProvider(this, 'AsgCapacityProvider', {
+            autoScalingGroup: autoScalingGroup,
+        });
+
+        cluster.addAsgCapacityProvider(capacityProvider);
+
         // Get an existing ECR repository
         const repository = ecr.Repository.fromRepositoryName(this, 'MyRepository', 'my-personal-website-repo');
 
-        // Create a load-balanced Fargate service and make it public
-        new ecs_patterns.ApplicationLoadBalancedFargateService(this, "PersonalWebsiteFargateService", {
-            cluster: cluster, // Required
-            desiredCount: 1,
-            taskImageOptions: {
-                image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
-                containerPort: 80,
-            },
+        // Create an ECS Task Definition
+        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
+
+        // Add a container to the Task Definition
+        const container = taskDefinition.addContainer('web', {
+            image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+            memoryLimitMiB: 512,
+            cpu: 512,
+        });
+
+        // Map port 80 of the container to port 80 on the host
+        container.addPortMappings({
+            containerPort: 80,
+            hostPort: 80
+        });
+
+        // Create an ECS Service using the EC2 launch type
+        new ecs.Ec2Service(this, 'EC2Service', {
+            cluster,
+            taskDefinition,
         });
     }
 }
--- lib/personal-website-with-flask-on-aws-stack.ts
+++ lib/personal-website-with-flask-on-aws-stack.ts
@@ -1,21 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import {AppStage} from './app-stage'
+import { AppStage } from './app-stage';
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class PersonalWebsiteWithFlaskOnAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create a custom IAM role for the pipeline
+    const pipelineRole = new Role(this, 'PipelineRole', {
+      assumedBy: new ServicePrincipal('codepipeline.amazonaws.com')
+    });
+
+    // Create the pipeline with the custom role
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'PersonalWebsiteWithFlaskOnAwsPipeline',
+      role: pipelineRole,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('Benjamintlj/personal-website-with-flask-on-aws', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
 
-    const prod = pipeline.addStage(new AppStage(this, 'prod', {
+    // Add stages to the pipeline
+    pipeline.addStage(new AppStage(this, 'prod', {
       env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION
