079da75769145ec34916f649aa145aac519bf625
n0928518 <n0928518@my.ntu.ac.uk>
2023-12-04 09:20:56 +0000

    route53 + public vpc

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -4,6 +4,8 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53_targets from "aws-cdk-lib/aws-route53-targets";
 
 export class EcsStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +14,12 @@ export class EcsStack extends cdk.Stack {
         // Create a new VPC
         const vpc = new ec2.Vpc(this, 'PersonalWebsiteVpc', {
             maxAzs: 2,
+            subnetConfiguration: [
+                {
+                    name: 'public',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                }
+            ],
         });
 
         // Create an ECS cluster
@@ -23,13 +31,32 @@ export class EcsStack extends cdk.Stack {
         const repository = ecr.Repository.fromRepositoryName(this, 'MyRepository', 'my-personal-website-repo');
 
         // Create a load-balanced Fargate service and make it public
-        new ecs_patterns.ApplicationLoadBalancedFargateService(this, "PersonalWebsiteFargateService", {
+        const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "PersonalWebsiteFargateService", {
             cluster: cluster, // Required
             desiredCount: 1,
             taskImageOptions: {
                 image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
                 containerPort: 80,
             },
+        });
+
+        const scaling = fargateService.service.autoScaleTaskCount({ maxCapacity: 1 });
+
+        // Retrieve the existing hosted zone
+        const hostedZone = route53.HostedZone.fromLookup(this, 'PersonalWebsiteHostedZone', {
+            domainName: 'benlewisjones.com',
+        });
+
+        // Create Records
+        new route53.ARecord(this, 'AliasRecord', {
+            zone: hostedZone,
+            target: route53.RecordTarget.fromAlias(new route53_targets.LoadBalancerTarget(fargateService.loadBalancer)),
+        });
+
+        new route53.CnameRecord(this, 'WwwRecord', {
+            zone: hostedZone,
+            domainName: fargateService.loadBalancer.loadBalancerDnsName,
+            recordName: 'www',
         });
     }
 }
